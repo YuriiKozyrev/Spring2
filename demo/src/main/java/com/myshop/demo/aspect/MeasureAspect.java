94bdc9548890105c1619a32d44859b8ca3c62ff0
Yurii Kozyrev <1000and39@gmail.com>
2020-12-21 13:52:24 +0300

    Lesson4

--- demo/src/main/java/com/myshop/demo/aspect/MeasureAspect.java
+++ demo/src/main/java/com/myshop/demo/aspect/MeasureAspect.java
@@ -4,7 +4,10 @@ import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.WebApplicationContext;
 
 import java.util.Date;
 import java.util.HashMap;
@@ -12,15 +15,18 @@ import java.util.Map;
 
 @Aspect
 @Component
+@Scope(value = WebApplicationContext.SCOPE_SESSION, proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class MeasureAspect {
 
-    Map<Date, String> hashMap = new HashMap<Date, String>();
+    final Map<Date, String> hashMap = new HashMap<Date, String>();
 
     @Pointcut("execution (* com.myshop.demo.controller..*.*(..))")
     public void getLogInfo(){
     }
 
+
     @After("getLogInfo()")
+
     public void logAround(JoinPoint jp) throws Throwable{
         Date date = new Date();
         hashMap.put(date, jp.getSignature().toShortString());
--- demo/src/main/java/com/myshop/demo/controller/ProductController.java
+++ demo/src/main/java/com/myshop/demo/controller/ProductController.java
@@ -2,10 +2,15 @@ package com.myshop.demo.controller;
 
 import com.myshop.demo.dto.ProductDto;
 import com.myshop.demo.service.ProductService;
+import com.myshop.demo.service.SessionObjectHolder;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.security.Principal;
@@ -18,11 +23,11 @@ public class ProductController {
 
 
     private final ProductService productService;
-//    private final SessionObjectHolder sessionObjectHolder;
+    private final SessionObjectHolder sessionObjectHolder;
 
-    public ProductController(ProductService productService) {
+    public ProductController(ProductService productService, SessionObjectHolder sessionObjectHolder) {
         this.productService = productService;
-//        this.sessionObjectHolder = sessionObjectHolder;
+        this.sessionObjectHolder = sessionObjectHolder;
     }
 
     @GetMapping
@@ -35,13 +40,28 @@ public class ProductController {
     }
 
     @GetMapping("/{id}/bucket")
-    public String addBucket(@PathVariable Long id, Principal principal){
+    public ResponseEntity<Void> addBucket(@PathVariable Long id, Principal principal){
+//    public String addBucket(@PathVariable Long id, Principal principal){
 //        sessionObjectHolder.addClick();
         if(principal == null){
-            return "redirect:/products";
+            return null;
         }
         productService.addToUserBucket(id, principal.getName());
-        return "redirect:/products";
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+//        return "redirect:/products";
+    }
+
+
+
+    @PostMapping
+    public ResponseEntity<Void> addProduct(ProductDto dto){
+        productService.addProduct(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @MessageMapping("/products")
+    public void messageAddProduct(ProductDto dto){
+        productService.addProduct(dto);
     }
 
 }
