9918322006e93035a5a69bb4f2008a2120ffef56
Yurii Kozyrev <1000and39@gmail.com>
2020-12-24 14:32:07 +0300

    Lesson5
    Add spring cloud config (use git repo: https://github.com/YuriiKozyrev/springcloud-config/tree/master/demo_shop)

---
+++ shop_modules/src/main/java/shop/ConfigCloudServerApp.java
@@ -0,0 +1,14 @@
+package shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCloudServerApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCloudServerApp.class, args);
+    }
+}
+
